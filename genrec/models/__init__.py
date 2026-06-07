052062233deada9892d1543cc6dc9004cc296759
TrinhHoangKhang <trinhhoangkhangvl@gmail.com>
2026-06-08 03:12:20 +0700

    update

--- genrec/models/__init__.py
+++ genrec/models/__init__.py
@@ -1,3 +1,3 @@
 from genrec.models.RPG.model import RPG
-from genrec.models.RPGUpgrade.model import RPGUpgrade
+# from genrec.models.RPGUpgrade.model import RPGUpgrade
 from genrec.models.RPGUpgrade_dpqEmbComp.model import RPGUpgrade_dpqEmbComp
